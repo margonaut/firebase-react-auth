1851a9c50783a93d21471c5d041036999d395c58
margonaut <margaret.morgane@gmail.com>
2016-09-04 15:18:34 -0400

    First sign in method

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import SignUpForm from './SignUpForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+
+    this.handleAuthChange = this.handleAuthChange.bind(this);
+  }
+
+  componentWillMount() {
+    this.auth = firebase.auth()
+    this.auth.onAuthStateChanged(user => {
+      this.handleAuthChange(user)
+    });
+  }
+
+  handleAuthChange(user) {
+    console.log("Auth change: ", user);
+    this.setState({ currentUser: user })
+  }
+
+
   render() {
+    var content;
+
+    if (this.currentUser) {
+      content = <p>Logged in!</p>
+    } else {
+      content = <SignUpForm auth={this.auth} />
+    }
+
     return (
       <div className="App">
-      Hello world
+      { content }
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+// userLoggedIn() {
+//   // this.auth.signInWithEmailAndPassword(email, pass);
+//   // // returns a promise with user
+//   //
+//   // this.auth.createUserWithEmailAndPassword(email, pass);
+//   // // creates and logs in user, return a promise.
+//   // // Promises only resolve one time
+//   //
+//
+// }
---
+++ src/components/SignUpForm.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+
+class SignUpForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    }
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  //
+  // componentWillMount() {
+  //   this.auth = firebase.auth()
+  // }
+
+  handleLogin(event) {
+    event.preventDefault();
+    let email = this.state.email;
+    let password = this.state.password;
+    const loginPromise = this.props.auth.signInWithEmailAndPassword(email, password);
+    loginPromise.catch(e => console.log(e.message));
+
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handlePasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+
+  render() {
+    return (
+      <form>
+        <input id="email" onChange={this.handleEmailChange} type="email" placeholder="email" />
+        <input id="password" onChange={this.handlePasswordChange} type="password" placeholder="password" />
+        <button id="signUpButton"></button>
+        <button onClick={this.handleLogin} id="loginButton">Login</button>
+      </form>
+    )
+  }
+}
+
+export default SignUpForm;
