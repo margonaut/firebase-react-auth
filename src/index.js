016527be1e7e0e644c072f3b24c952d0633a170d
margonaut <margaret.morgane@gmail.com>
2016-09-04 14:17:27 -0400

    Install firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './firebaseConfig';
 import App from './components/App';
 import './index.css';
+
+var config = {
+     apiKey: firebaseConfig.apiKey,
+     authDomain: firebaseConfig.authDomain,
+     databaseURL: firebaseConfig.databaseURL
+   };
+
+firebase.initializeApp(config)
 
 ReactDOM.render(
   <App />,
